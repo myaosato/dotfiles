12748c041a8d19437d7e02a4e8645b9409de31ce
myaosato <tetu60u@yahoo.co.jp>
2025-09-06 13:59:42 +0900

    add private.bash

--- files/rc.bash
+++ files/rc.bash
@@ -65,3 +65,6 @@ if [ -d "$HOME/.nvm" ]; then
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 fi
+
+# private (secret or experimental) settings
+[ -f "$HOME/.dotfiles/files/private.bash" ] && . $HOME/.dotfiles/files/private.bash
